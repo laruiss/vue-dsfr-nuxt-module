57db70780748084a4b4445981af3d0c12f51fefb
Stanislas Ormières <stan@stormier.ninja>
2023-12-17 15:10:46 +0100

    feat: :sparkles: configure le module pour VueDsfr

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,21 @@
+import * as icons from './icons'
+// import '@gouvfr/dsfr/dist/utility/utility.main.min.css'
+
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      vueDsfr: {
+        icons: Object.values(icons),
+      }
+    },
+  },
+  css: [
+    '@gouvfr/dsfr/dist/core/core.main.min.css',
+    '@gouvfr/dsfr/dist/component/component.main.min.css',
+    '@gouvfr/dsfr/dist/utility/utility.main.min.css',
+    '@gouvfr/dsfr/dist/scheme/scheme.min.css',
+    '@gouvfr/dsfr/dist/utility/icons/icons.main.min.css',
+  ],
   modules: ['../src/module'],
-  myModule: {},
   devtools: { enabled: true }
 })
